707a3c41321e2f48abc9f3ceed8ffc7e031d8a4b
imulan <18570354653@163.com>
2026-04-16 21:41:44 +0800

    初始项目

---
+++ src/main/java/top/imulan/webservice/WebserviceDemoApplication.java
@@ -0,0 +1,16 @@
+package top.imulan.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author imulan
+ */
+@SpringBootApplication
+public class WebserviceDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebserviceDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/imulan/webservice/WebserviceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package top.imulan.webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebserviceDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
